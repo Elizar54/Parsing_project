2561b8bae87e04e2edab35659c1d25c1760f9f6a
LeonoffNSU <k.leonov@g.nsu.ru>
2023-10-09 21:46:33 +0700

    items

--- main.py
+++ main.py
@@ -55,4 +55,56 @@ print(list_of_links)
 print(len(list_of_links))
 
 # здесь начну код по парсингу теперь уже карточек товаров
-url_general = 'https://www.lamoda.ru'
+url_item = 'https://www.lamoda.ru/p/rtlacq549202/shoes-reebok-kedy/'
+response = requests.get(url_item)
+
+# основные данные: бренд, наименование, цена, артикул
+item_html = response.text
+data_index = item_html.rfind('<script type="application/ld+json">')
+item_html = item_html[data_index:]
+index_of_left_bracket = item_html.find('{')
+index_of_right_bracket = item_html.rfind('}')
+item_html = item_html[index_of_left_bracket : index_of_right_bracket + 1]
+
+dict_data = json.loads(item_html)
+
+name_crude = dict_data['name']
+name_crude = name_crude.replace('&quot;','',1)
+brand = name_crude[0 : name_crude.find('&')]
+
+name_crude = name_crude[name_crude.find('&'):]
+name_crude = name_crude.replace('&quot;', '')
+name = name_crude.strip()
+
+price = int(float(dict_data['offers']['price']))
+
+vendor_code = dict_data['offers']['sku']
+
+# можно выводить основные данные
+print(brand)
+print(name)
+print(price)
+print(vendor_code)
+
+# теперь парсим страну-производителя, здесь парсер абсолютно конченый
+item_html_country = response.text
+item_html_country = item_html_country[item_html_country.find('<div id="modals">'):]
+item_html_country = item_html_country[item_html_country.find('<script>'):]
+item_html_country = item_html_country[0 : item_html_country.find('</script>')]
+item_html_country = item_html_country[item_html_country.find('{') : item_html_country.rfind('}') + 1]
+item_html_country = item_html_country[item_html_country.find('Страна производства'):]
+item_html_country = item_html_country[item_html_country.find(':') + 1:]
+item_html_country = item_html_country[0 : item_html_country.find('}')]
+country = item_html_country.strip('"')
+print(country)
+
+# парсим цену без скидки (для товаров без акций получится обычная цена) и определяем саму скидку
+item_html_discount = response.text
+item_html_discount = item_html_discount[item_html_discount.find('span class="x-premium-product-prices__price "'):]
+item_html_discount = item_html_discount[item_html_discount.find('content'):]
+item_html_discount = item_html_discount[item_html_discount.find('"') + 1:]
+item_html_discount = item_html_discount[0 : item_html_discount.find('"')]
+price_without_discount = int(float(item_html_discount))
+
+discount = '{:.0%}'.format(1 - price / price_without_discount)
+print(discount)
